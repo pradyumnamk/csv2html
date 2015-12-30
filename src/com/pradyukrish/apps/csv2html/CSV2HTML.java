ab64c5336c3f9ca5d96d447c91ca64f1c82a3729
pradyumnamk <pradyumna.mokshagundam@kony.com>
2015-12-30 18:41:07 +0530

    Added CSV2HTML.java
    Added functionality

--- src/com/pradyukrish/apps/csv2html/CSV2HTML.java
+++ src/com/pradyukrish/apps/csv2html/CSV2HTML.java
@@ -1,7 +1,21 @@
 public class CSV2HTML
 {
+	
+	private String csvData;
+	
 	public CSV2HTML()
 	{
 		System.out.println("CSV2HTML instantiated!!!");
+		csvData = "This is an empty CSV2HTML object";
+	}
+	
+	public CSV2HTML(String csvData)
+	{
+		this.csvData = csvData;
+	}
+	
+	public String toString()
+	{
+		return csvData; 
 	}
 }
